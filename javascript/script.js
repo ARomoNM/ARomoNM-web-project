81611bf71f36715c87566e11d37dd865c38b634c
ARomoNM <aromo@nmsu.edu>
2026-05-05 22:53:36 -0600

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,23 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("golfForm");
 
+  form.addEventListener("submit", function (event) {
+    event.perventDefault();
+
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let phone = document.getElementById("phone").value;
+    let course = document.getElementById("course").value;
+    let date = document.getElementById("course").value;
+
+    document.getElementById("responseMessage").innerHTML = 
+      'Thanks ${name}!<br>
+      I'll contact you at ${email} or ${phone}<br>
+      for ${course} before ${date} to confirm you are still interested in playing.';
+
+    form.reset();
+  });
+    const dateInput = document.getElementById("date");
+    datInput.min = new Date().toISOString().split(*T")[0];
+
+  });
