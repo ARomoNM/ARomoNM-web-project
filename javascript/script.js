a15fae1510fe67e31057c9170d2ef0403b151732
ARomoNM <aromo@nmsu.edu>
2026-05-05 23:34:25 -0600

    Changes

--- javascript/script.js
+++ javascript/script.js
@@ -11,9 +11,9 @@ document.addEventListener("DOMContentLoaded", function () {
     let date = document.getElementById("date").value;
 
     document.getElementById("responseMessage").innerHTML = 
-      `Thanks ${name}!<br>
-      I'll contact you at ${email} or ${phone}<br>
-      for ${course} before ${date} to confirm you are still interested in playing.`;
+      "Thanks " + name + "!<br>" +
+      "I'll contact you at " + email + " or " + phone + "<br>" +
+      "for " + course + " on " + date + " to confirm you are still interested in playing.";
 
     form.reset();
   });
